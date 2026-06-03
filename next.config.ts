6dbfaf4c3b8d6b9d65900024e46e6abba8367cb4
Top Noch Wealth <bleupitts@gmail.com>
2026-06-02 20:27:11 -0700

    Move redirects to next.config to fire before prerender

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "topnochwealth.com" }],
+        destination: "https://topnochenterprises.com/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.topnochwealth.com" }],
+        destination: "https://topnochenterprises.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
